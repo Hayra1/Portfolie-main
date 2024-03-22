1897b96d9f248d99ca49c7173f596e7d653679a9
foxy7578 <sergeyhayrapetyan@hotmail.com>
2024-03-22 23:47:20 +0100

    changes 6

---
+++ Portfolie/java.js
@@ -0,0 +1,12 @@
+// window.onscroll = function(){myfunction ()};
+
+// var navbar = document.querySelector(".div-nav");
+// var sticky = navbar.offsetTop;
+
+// function myfunction() {
+//   if (window.scrollY >= sticky) {
+//     navbar.classList.add("sticky");
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
